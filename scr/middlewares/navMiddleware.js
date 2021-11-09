1387c342043941d88034cd95ffb78040561327f6
Reneta <renkagh55@gmail.com>
2021-11-09 07:53:25 +0200

    pages&reg form

---
+++ scr/middlewares/navMiddleware.js
@@ -0,0 +1,8 @@
+// import { render } from "../../node_modules/lit-html/lit-html.js";
+// import { renderNavigation } from "../views/navView.js";
+
+// const navElement = document.getElementById(".navigation");
+// export function navMiddleware(ctx, next) {
+//   render(renderNavigation(ctx), navElement);
+//   next();
+// }
---
+++ scr/views/navView.js
@@ -0,0 +1,23 @@
+// import { html } from "../../node_modules/lit-html/lit-html.js";
+// const navTemplate = () => html`
+//   <nav>
+//     <ul class="nav">
+//       <li class="nav-item">
+//         <a class="nav-link active" aria-current="page" href="#">Active</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="#">Link</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="#">Link</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link disabled">Disabled</a>
+//       </li>
+//     </ul>
+//   </nav>
+// `;
+// export const renderNavigation = (ctx) => {
+//   //to do..
+//   return navTemplate();
+// };
