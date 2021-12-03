48b69730425509d9cb4f0863176b0372e4c5df14
Reneta <renkagh55@gmail.com>
2021-12-03 22:38:12 +0200

    --

--- app.js
+++
@@ -1,6 +0,0 @@
-// import page from "./node_modules/page/page.mjs";
-// import { navMiddleware } from "./scr/middlewares/navMiddleware.js";
-
-// page(navMiddleware);
-// page("/", () => console.log("home page"));
-// page.start();
